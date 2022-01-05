aea10415efa5b3dece11c816b3ea4cddb0eff964
Olga T. Schubert <olga.schubert@gmail.com>
2022-01-06 00:21:41 +0100

    Update BE21-05_Fitness-vs-protein.R plots

--- BE21_FitnessScreen/code/BE21-05_Fitness-vs-protein.R
+++ BE21_FitnessScreen/code/BE21-05_Fitness-vs-protein.R
@@ -63,7 +63,7 @@ fitnessVsSpecificityPlot.gd <- function(input, log2fc, ess = F) {
                 
         } else {
                 input %>%
-                        mutate(essential = ifelse(essential == TRUE, "essential", "nonessential"),
+                        mutate(essential = ifelse(essential == TRUE, "Essential", "Nonessential"),
                                essential = forcats::fct_rev(as.factor(essential))) %>%
                         ggplot(aes(x = FDR0.05_count, y = {{log2fc}}, color = essential)) +
                         geom_point(position = position_jitterdodge()) +
@@ -99,49 +99,79 @@ ggsave(plot = p6, paste0(resdir, "Fitness-vs-protSpecificity_gd_ess_24h.pdf"), w
 
 
 
-
-
 # Combined plot ----------------------------------------------------------------
 
-(p6a <- comb.gd %>%
-        mutate(FDR0.05_count = str_c("n", FDR0.05_count)) %>%
-        mutate(FDR0.05_count = ifelse(FDR0.05_count %in% c("n8", "n9", "n10"), "n8+", FDR0.05_count)) %>%
-        mutate(essential = ifelse(essential == TRUE, "Essential", "Nonessential"),
-               essential = forcats::fct_rev(as.factor(essential))) %>%
-        ggplot(aes(x = FDR0.05_count, y = T24h_log2fc, color = essential)) +
-        geom_point(position = position_jitterdodge()) +
-        geom_boxplot(alpha = 0.8, outlier.shape = NA, 
-                     position = "dodge", show.legend = F) +
-        scale_color_manual(values = c("darkseagreen3", "darkseagreen4")) +
-        coord_cartesian(ylim = c(-2.5, 2)) +
-        labs(x = "Number of proteins affected",
-             y = "Log2FC") +
-        theme_bw() +
-        theme(legend.title = element_blank(),
-              panel.grid.major = element_blank(), 
-              panel.grid.minor = element_blank()))
-
 (p7 <- comb.gd %>%
-                mutate(FDR0.05_count = str_c("n", FDR0.05_count)) %>%
-                mutate(FDR0.05_count = ifelse(FDR0.05_count %in% c("n8", "n9", "n10"), "n8+", FDR0.05_count)) %>%
+         mutate(FDR0.05_count_bin = str_c("n", FDR0.05_count)) %>%
+         mutate(FDR0.05_count_bin = ifelse(FDR0.05_count_bin %in% c("n8", "n9", "n10"), "n8+", FDR0.05_count_bin)) %>%
+         mutate(essential = ifelse(essential == TRUE, "Essential", "Nonessential"),
+                essential = forcats::fct_rev(as.factor(essential))) %>%
+         ggplot() +
+         geom_point(aes(x = FDR0.05_count_bin, y = T24h_log2fc, color = essential),
+                    position = "jitter", alpha = 0.7, show.legend = F) +
+         geom_boxplot(aes(x = FDR0.05_count_bin, y = T24h_log2fc),
+                      alpha = 0.8, outlier.shape = NA, show.legend = F) +
+         ggpubr::stat_cor(aes(x = FDR0.05_count, y = T24h_log2fc),
+                          label.x.npc = 0.38, label.y.npc = 0.82) +
+         scale_color_manual(values = c("darkseagreen3", "darkseagreen4")) +
+         coord_cartesian(ylim = c(-2.5, 1)) +
+         labs(x = "Number of proteins affected",
+              y = "Fitness (Log2FC)") +
+         theme_bw() +
+         theme(legend.title = element_blank(),
+               panel.grid.major = element_blank(), 
+               panel.grid.minor = element_blank()))
+
+(p8 <- comb.gd %>%
+                mutate(FDR0.05_count_bin = str_c("n", FDR0.05_count)) %>%
+                mutate(FDR0.05_count_bin = ifelse(FDR0.05_count_bin %in% c("n8", "n9", "n10"), "n8+", FDR0.05_count)) %>%
                 mutate(essential = ifelse(essential == TRUE, "Essential", "Nonessential"),
                        essential = forcats::fct_rev(as.factor(essential))) %>%
-                ggplot(aes(x = FDR0.05_count, fill = essential)) +
+                ggplot(aes(x = FDR0.05_count_bin, fill = essential)) +
                 geom_bar(position = "fill") +
                 scale_fill_manual(values = c("darkseagreen3", "darkseagreen4")) +
                 scale_y_continuous(breaks = c(0, 0.5, 1)) +
-                labs(x = "Number of proteins affected", y = "Fraction") +
+                labs(x = "Number of proteins affected", y = "Fraction of gRNAs") +
                 theme_bw() +
                 theme(legend.title = element_blank(),
                       panel.grid.major = element_blank(), 
                       panel.grid.minor = element_blank()))
 
-ggarrange(p6 + rremove("xlab") + rremove("x.text") + rremove("x.ticks"), 
-          NULL, 
-          p7, 
-          ncol = 1, heights = c(2, -0.3, 1), widths = c(1, 1, 1), align = "hv",
+ggarrange(p7 + rremove("xlab") + rremove("x.text") + rremove("x.ticks"), NULL, p8, 
+          ncol = 1, heights = c(2, -0.3, 1.5), widths = c(1, 1, 1), align = "hv",
           common.legend = T, legend = "right", legend.grob = get_legend(p7))
-ggsave(paste0(resdir, "Fitness-vs-protSpecificity_gd_ess_24h_WithFraction.pdf"), width = 4.5, height = 4)
+ggsave(paste0(resdir, "Fitness-vs-protSpecificity_gd_24h_WithFraction.pdf"), width = 4.5, height = 4.5)
+
+
+counts <- comb.gd %>%
+        mutate(bin = case_when(FDR0.05_count <3 ~ "spec",
+                               FDR0.05_count >2 ~ "broad",
+                               TRUE ~ NA_character_)) %>%
+        mutate(essential = ifelse(essential == TRUE, "Essential", "Nonessential"),
+               essential = forcats::fct_rev(as.factor(essential))) %>%
+        filter(!is.na(bin)) %>%
+        group_by(bin, essential) %>%
+        summarize(n = n()) %>% 
+        print()
+
+# bin   essential        n
+# broad Nonessential    67
+# broad Essential      194
+# spec  Nonessential 10840
+# spec  Essential     5249
+
+# Fraction of essential genes among genes that upon perturbation affect 3 or more proteins:
+194/(194+67) # 74%
+
+# Fraction of essential genes among genes that upon perturbation affect less than 3 proteins:
+5249/(5249+10840) # 33%
+
+fisher.test(matrix(counts$n, ncol = 2))
+# p-value < 2.2e-16
+# odds ratio = 0.1672554 
+
+chisq.test(matrix(counts$n, ncol = 2))
+# X-squared = 199.27, df = 1, p-value < 2.2e-16
 
 
 
