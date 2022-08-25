45b4f843c936d23087ca128f8559221ccc2fd1ed
Olga T. Schubert <olga.schubert@gmail.com>
2022-08-25 14:58:05 +0200

    Update fitness-vs-protein plots

--- BE21_FitnessScreen/code/BE21-05_Fitness-vs-protein.R
+++ BE21_FitnessScreen/code/BE21-05_Fitness-vs-protein.R
@@ -41,88 +41,34 @@ write_csv(comb.gd, paste0(resdir, "combdf_gd_fit-prot.csv"))
 
 
 
-# Plotting fitness as a function of specificity --------------------------------
+# Plots ------------------------------------------------------------------------
 
-fitnessVsSpecificityPlot.gd <- function(input, log2fc, ess = F) {
-        
-        input <- input %>%
-                mutate(FDR0.05_count = str_c("n", FDR0.05_count)) %>%
-                mutate(FDR0.05_count = ifelse(FDR0.05_count %in% c("n8", "n9", "n10"), "n8+", FDR0.05_count))
-
-        if(ess == F) {
-                input %>%
-                        ggplot(aes(x = FDR0.05_count, y = {{log2fc}})) +
-                        geom_point(position = "jitter", alpha = 0.3) +
-                        geom_boxplot(alpha = 0.8, outlier.shape = NA) +
-                        coord_cartesian(ylim = c(-2.5, 1)) +
-                        labs(x = "Number of proteins affected",
-                             y = "Log2FC") +
-                        theme_bw() +
-                        theme(panel.grid.major = element_blank(), 
-                              panel.grid.minor = element_blank())
-                
-        } else {
-                input %>%
-                        mutate(essential = ifelse(essential == TRUE, "Essential", "Nonessential"),
-                               essential = forcats::fct_rev(as.factor(essential))) %>%
-                        ggplot(aes(x = FDR0.05_count, y = {{log2fc}}, color = essential)) +
-                        geom_point(position = position_jitterdodge()) +
-                        geom_boxplot(alpha = 0.8, outlier.shape = NA, position = "dodge") +
-                        scale_color_manual(values = c("darkseagreen3", "darkseagreen4")) +
-                        coord_cartesian(ylim = c(-2.5, 1)) +
-                        labs(x = "Number of proteins affected",
-                             y = "Log2FC") +
-                        theme_bw() +
-                        theme(legend.title = element_blank(),
-                              panel.grid.major = element_blank(), 
-                              panel.grid.minor = element_blank())
-        }
-}
-
-(p1 <- fitnessVsSpecificityPlot.gd(input = comb.gd, log2fc = T00h_log2fc, ess = F))
-(p2 <- fitnessVsSpecificityPlot.gd(input = comb.gd, log2fc = T08h_log2fc, ess = F))
-(p3 <- fitnessVsSpecificityPlot.gd(input = comb.gd, log2fc = T24h_log2fc, ess = F))
-
-(p123 <- ggarrange(p1, p2, p3, ncol = 3, nrow = 1))
-ggsave(plot = p123, paste0(resdir, "Fitness-vs-protSpecificity_gd.pdf"), width = 9, height = 3)
-ggsave(plot = p3, paste0(resdir, "Fitness-vs-protSpecificity_gd_24h.pdf"), width = 3, height = 3)
-
-
-(p4 <- fitnessVsSpecificityPlot.gd(input = comb.gd, log2fc = T00h_log2fc, ess = T))
-(p5 <- fitnessVsSpecificityPlot.gd(input = comb.gd, log2fc = T08h_log2fc, ess = T))
-(p6 <- fitnessVsSpecificityPlot.gd(input = comb.gd, log2fc = T24h_log2fc, ess = T))
-
-(p456 <- ggarrange(p4, p5, p6, ncol = 3, nrow = 1))
-ggsave(plot = p456, paste0(resdir, "Fitness-vs-protSpecificity_gd_ess.pdf"), width = 14, height = 3)
-ggsave(plot = p6, paste0(resdir, "Fitness-vs-protSpecificity_gd_ess_24h.pdf"), width = 4.5, height = 3)
-
-
-
-
-# Combined plot ----------------------------------------------------------------
-
-(p7 <- comb.gd %>%
+(p1 <- comb.gd %>%
          mutate(FDR0.05_count_bin = str_c("n", FDR0.05_count)) %>%
          mutate(FDR0.05_count_bin = ifelse(FDR0.05_count_bin %in% c("n8", "n9", "n10"), "n8+", FDR0.05_count_bin)) %>%
          mutate(essential = ifelse(essential == TRUE, "Essential", "Nonessential"),
                 essential = forcats::fct_rev(as.factor(essential))) %>%
          ggplot() +
          geom_point(aes(x = FDR0.05_count_bin, y = T24h_log2fc, color = essential),
-                    position = "jitter", alpha = 0.7, show.legend = F) +
+                    position = "jitter", alpha = 0.7, show.legend = T) +
          geom_boxplot(aes(x = FDR0.05_count_bin, y = T24h_log2fc),
                       alpha = 0.8, outlier.shape = NA, show.legend = F) +
          ggpubr::stat_cor(aes(x = FDR0.05_count, y = T24h_log2fc),
                           label.x.npc = 0.38, label.y.npc = 0.82) +
          scale_color_manual(values = c("darkseagreen3", "darkseagreen4")) +
          coord_cartesian(ylim = c(-2.5, 1)) +
-         labs(x = "Number of proteins affected",
-              y = "Fitness (Log2FC)") +
+         labs(x = "Number of proteins affected by gRNA",
+              y = "Fitness effect of gRNA (Log2FC)",
+              color = "gRNA target gene") +
          theme_bw() +
-         theme(legend.title = element_blank(),
+         theme(#legend.position = "none",
+               #legend.title = element_blank(),
                panel.grid.major = element_blank(), 
                panel.grid.minor = element_blank()))
+ggsave(paste0(resdir, "Fitness-vs-protSpecificity_gd_24h.pdf"), width = 5, height = 3)
 
-(p8 <- comb.gd %>%
+
+(p2 <- comb.gd %>%
                 mutate(FDR0.05_count_bin = str_c("n", FDR0.05_count)) %>%
                 mutate(FDR0.05_count_bin = ifelse(FDR0.05_count_bin %in% c("n8", "n9", "n10"), "n8+", FDR0.05_count)) %>%
                 mutate(essential = ifelse(essential == TRUE, "Essential", "Nonessential"),
@@ -131,17 +77,26 @@ ggsave(plot = p6, paste0(resdir, "Fitness-vs-protSpecificity_gd_ess_24h.pdf"), w
                 geom_bar(position = "fill") +
                 scale_fill_manual(values = c("darkseagreen3", "darkseagreen4")) +
                 scale_y_continuous(breaks = c(0, 0.5, 1)) +
-                labs(x = "Number of proteins affected", y = "Fraction of gRNAs") +
+                labs(x = "Number of proteins affected by gRNA", 
+                     y = "Fraction of gRNAs",
+                     fill = "gRNA target gene") +
                 theme_bw() +
-                theme(legend.title = element_blank(),
+                theme(#legend.title = element_blank(),
                       panel.grid.major = element_blank(), 
                       panel.grid.minor = element_blank()))
+ggsave(paste0(resdir, "Fraction-vs-protSpecificity_gd_24h.pdf"), width = 5, height = 2)
 
-ggarrange(p7 + rremove("xlab") + rremove("x.text") + rremove("x.ticks"), NULL, p8, 
+
+# Combined plot
+ggarrange(p1 + rremove("xlab") + rremove("x.text") + rremove("x.ticks"), NULL, p2, 
           ncol = 1, heights = c(2, -0.3, 1.5), widths = c(1, 1, 1), align = "hv",
-          common.legend = T, legend = "right", legend.grob = get_legend(p7))
-ggsave(paste0(resdir, "Fitness-vs-protSpecificity_gd_24h_WithFraction.pdf"), width = 4.5, height = 4.5)
+          common.legend = T, legend = "right", legend.grob = get_legend(p2))
+ggsave(paste0(resdir, "Fitness-vs-protSpecificity_gd_24h_WithFraction.pdf"), width = 5, height = 4.5)
 
+
+
+
+# Counts -----------------------------------------------------------------------
 
 counts <- comb.gd %>%
         mutate(bin = case_when(FDR0.05_count <3 ~ "spec",
@@ -172,6 +127,48 @@ fisher.test(matrix(counts$n, ncol = 2))
 
 chisq.test(matrix(counts$n, ncol = 2))
 # X-squared = 199.27, df = 1, p-value < 2.2e-16
+
+
+
+
+# Reads per gRNA at "pre" time point -------------------------------------------
+# Addressing concern by reviewer that the initial frequencies of the n6/n7/n8+ 
+# bins are already skewed/lower and therefore drop out more due to stochastic
+# effects.
+
+# Import fitness data with more info
+fitn.gd2 <- readRDS("results/fitness_all4/all4_Ratios_gd_allInfo.RDS") %>% print()
+
+
+# Combine protein and fitness data
+comb.gd2 <- prot.gd %>%
+        left_join(fitn.gd2, by = "guide") %>% 
+        print()
+
+comb.gd2 %>%
+                mutate(FDR0.05_count_bin = str_c("n", FDR0.05_count)) %>%
+                mutate(FDR0.05_count_bin = ifelse(FDR0.05_count_bin %in% c("n8", "n9", "n10"), "n8+", FDR0.05_count_bin)) %>%
+                mutate(essential = ifelse(essential == TRUE, "Essential", "Nonessential"),
+                       essential = forcats::fct_rev(as.factor(essential))) %>%
+                ggplot() +
+                geom_point(aes(x = FDR0.05_count_bin, y = Tpre_A, color = essential),
+                           position = "jitter", alpha = 0.7) +
+                geom_point(aes(x = FDR0.05_count_bin, y = Tpre_B, color = essential),
+                           position = "jitter", alpha = 0.7) +
+                geom_point(aes(x = FDR0.05_count_bin, y = Tpre_C, color = essential),
+                           position = "jitter", alpha = 0.7) +
+                geom_point(aes(x = FDR0.05_count_bin, y = Tpre_D, color = essential),
+                           position = "jitter", alpha = 0.7) +
+                geom_boxplot(aes(x = FDR0.05_count_bin, y = Tpre_A),
+                             alpha = 0.8, outlier.shape = NA, show.legend = F) +
+                coord_cartesian(ylim = c(0, 2000)) +
+                labs(x = "Number of proteins affected by gRNA",
+                     y = "Number of reads per gRNA",
+                     color = "gRNA target gene") +
+                theme_bw() +
+                theme(panel.grid.major = element_blank(), 
+                      panel.grid.minor = element_blank())
+ggsave(paste0(resdir, "Reads-vs-protSpecificity_gd_preGalInd_boxplotA.pdf"), width = 5, height = 3)
 
 
 
